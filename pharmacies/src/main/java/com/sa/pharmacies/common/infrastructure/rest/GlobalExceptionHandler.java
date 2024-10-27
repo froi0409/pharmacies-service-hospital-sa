d96c7902a712e9bd08b5e9c42627e8860208d5c8
chejohrpp <sergioroherp@gmail.com>
2024-10-26 20:40:03 -0600

    deprecated globalexception:

--- pharmacies/src/main/java/com/sa/pharmacies/common/infrastructure/rest/GlobalExceptionHandler.java
+++ pharmacies/src/main/java/com/sa/pharmacies/common/infrastructure/rest/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@Deprecated
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
