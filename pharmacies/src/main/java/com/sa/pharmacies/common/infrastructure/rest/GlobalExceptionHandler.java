af6bd30dc7aef34f1cf31262bd2cd78b971fad89
chejohrpp <sergioroherp@gmail.com>
2024-10-26 20:42:17 -0600

    jjdf

--- pharmacies/src/main/java/com/sa/pharmacies/common/infrastructure/rest/GlobalExceptionHandler.java
+++ pharmacies/src/main/java/com/sa/pharmacies/common/infrastructure/rest/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Deprecated
+
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
