caa55327780f2a6b08c3a7a4738ac9d26caaee34
chejohrpp <sergioroherp@gmail.com>
2024-10-26 20:30:58 -0600

    embeddedKafka

--- pharmacies/src/test/java/com/sa/pharmacies/PharmaciesApplicationTests.java
+++ pharmacies/src/test/java/com/sa/pharmacies/PharmaciesApplicationTests.java
@@ -2,8 +2,10 @@ package com.sa.pharmacies;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka
 class PharmaciesApplicationTests {
 
 	@Test
